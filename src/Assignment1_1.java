95e72541848dc1751d2f2c704bb378d4319dfb8f
adamperrry <adamperrry@gmail.com>
2020-11-13 11:21:05 -0600

    Initial commit

---
+++ src/Assignment1_1.java
@@ -0,0 +1,10 @@
+public class Assignment1_1 {
+    public static void main(String[] arg) {
+        String name = "Adam Perry";
+        String course = "Java Web";
+        String hometown = "West Point, NE";
+        String dessert = "Mom's Cheesecake";
+
+        System.out.println(name + "\n" + course + "\n" + hometown + "\n" + dessert);
+    }
+}
---
+++ src/Assignment1_2.java
@@ -0,0 +1,10 @@
+public class Assignment1_2 {
+    public static void main(String[] arg) {
+        String line1 = "     J    A    V     V    A   ";
+        String line2 = "     J   A A    V   V    A A  ";
+        String line3 = "J    J  AAAAA    V V    AAAAA ";
+        String line4 = " J  J  A     A    V    A     A";
+        
+        System.out.println(line1 + "\n" + line2  + "\n" + line3 + "\n" + line4);
+    }
+}
